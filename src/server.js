6f059641d7721b6d7d7fde079f21a0808850353f
Dario Reis <guttyerresjunior1995@gmail.com>
2025-08-21 19:47:26 -0300

    feat(http): add request method identification and status code handling
    Implemented functionality to identify request methods (GET, POST, etc.) and return responses with appropriate status codes. Added support for custom messages such as 201 Created, 404 Not Found, and 500 Internal Server Error. Enhanced response headers to include metadata and improved status code visualization with color distinction.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import http from "node:http"
 const server = http.createServer((request, response) => {
     const {method} = request
 
-    return response.end("Método " + method)
+    return response.writeHead(201).end("Criado com sucesso!")
 })
 
 server.listen(9374)
