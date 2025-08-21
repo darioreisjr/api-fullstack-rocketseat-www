ee06e67c39f0ab8321c8480f1c85d08100b0c606
Dario Reis <guttyerresjunior1995@gmail.com>
2025-08-21 20:02:25 -0300

    feat(http): add request URL access to server responses
    Implemented functionality to access and log the request URL from incoming requests. This allows better debugging, monitoring, and customization of server responses based on the requested route.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-    const {method} = request
+    const { url, method } = request
 
-    return response.writeHead(201).end("Criado com sucesso!")
+    if (method === "GET" && url === "/products") {
+        return response.end("Lista de produtos!")
+    }
+
+    if (method === "POST" && url === "/products") {
+        return response.writeHead(201).end("Produto cadastrado!")
+    }
+
+    return response.writeHead(400).end("Rota não encontrada!")
 })
 
 server.listen(9374)
